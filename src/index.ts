78dcc8b8e9f304ba98594c82f03244cad5d328f5
awer000 <awer000@naver.com>
2021-08-15 23:43:46 +0900

    feat: 3d 마우스 감지 테스팅 성공

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,69 @@
 import * as THREE from 'three';
 
-const draw = () => {
-    const canvas = document.getElementById('jigsaw');
-    const renderer = new THREE.WebGLRenderer({ canvas });
 
-    const fov = 90;
-    const aspect = 1;  // the canvas default
-    const near = 0.1;
-    const far = 10;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 10;
+const mouse = new THREE.Vector2();
+const canvas = document.getElementById('jigsaw');
 
-    const scene = new THREE.Scene();
+const fov = 90;
+const aspect = 1;  // the canvas default
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 10;
 
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const scene = new THREE.Scene();
 
-    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const material = new THREE.MeshBasicMaterial({ color: 'pink' });  // greenish blue
 
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-    renderer.render(scene, camera);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.render(scene, camera);
 
+const raycaster = new THREE.Raycaster();
+let selected
+
+function onMouseMove(event) {
+    event.preventDefault();
+
+    const offset = document.getElementById('jigsaw').getBoundingClientRect();
+    const pos = {
+        x: event.pageX - offset.left,
+        y: event.pageY - offset.top
+    };
+
+    mouse.x = (pos.x / 1024) * 2 - 1;
+    mouse.y = -(pos.y / 1024) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(scene.children);
+
+
+    for (let i = 0; i < intersects.length; i++) {
+        if (intersects[i].object) {
+            selected = intersects[i].object
+            selected.material.color.set('blue')
+        }
+    }
+
+    if (!intersects.length && selected) {
+        selected.material.color.set('pink')
+    }
 }
 
-draw();
+const draw = () => {
+    requestAnimationFrame(draw);
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('mousemove', onMouseMove, false);
+window.requestAnimationFrame(draw);
+
+
+
